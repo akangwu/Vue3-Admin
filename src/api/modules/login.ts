1d7d47d3ac9160ba448d59a02dcc67590bbad2f8
wujkb <wujkb@yonyou.com>
2024-03-06 11:08:56 +0800

    format：格式化代码修复

--- src/api/modules/login.ts
+++ src/api/modules/login.ts
@@ -1,8 +1,8 @@
-import http from '@/api'
-import {PORT1} from '@/api/config/servicePort'
-import {Login} from '@/api/interface/index'
-import authButtonList from '@/assets/json/authButtonList.json'
-import authMenuList from '@/routers/localRouterList.json'
+import http from "@/api";
+import { PORT1 } from "@/api/config/servicePort";
+import { Login } from "@/api/interface/index";
+import authButtonList from "@/assets/json/authButtonList.json";
+import authMenuList from "@/routers/localRouterList.json";
 
 /**
  * @name 登录模块
